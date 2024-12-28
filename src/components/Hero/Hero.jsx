13bdccfee8cc071792a3e7fadf5261cc17e3d949
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2024-12-28 13:28:41 -0500

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Oba</h1>
+        <p className={styles.description}>
+        Undergraduate student at Morgan State University. I have a keen interest in software development, data science, and Machine Learning
+
+          
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/oba.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
