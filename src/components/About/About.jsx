0263d71642a9ef39f9b1a8df73149bae21fa5675
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2024-12-31 02:18:43 -0500

    final

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/obeya-modified.png")}
+          src={getImageUrl("about/gameface-modified.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
